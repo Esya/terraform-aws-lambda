55bcf9d3cbb5c359156a03e44f1c004bd7ec2289
Loren Gordon <lorengordon@users.noreply.github.com>
2018-06-25 08:38:20 -0400

    Simplifies lambda resource definition

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "function_arn" {
   description = "The ARN of the Lambda function"
-  value       = "${element(concat(aws_lambda_function.lambda.*.arn, aws_lambda_function.lambda_with_dl.*.arn, aws_lambda_function.lambda_with_vpc.*.arn, aws_lambda_function.lambda_with_dl_and_vpc.*.arn), 0)}"
+  value       = "${aws_lambda_function.lambda.arn}"
 }
 
 output "function_name" {
   description = "The name of the Lambda function"
-  value       = "${element(concat(aws_lambda_function.lambda.*.function_name, aws_lambda_function.lambda_with_dl.*.function_name, aws_lambda_function.lambda_with_vpc.*.function_name, aws_lambda_function.lambda_with_dl_and_vpc.*.function_name), 0)}"
+  value       = "${aws_lambda_function.lambda.function_name}"
 }
 
 output "role_arn" {
